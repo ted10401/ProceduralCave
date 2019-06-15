4cf72214c20814ea75f40ca206d193006e7153cb
ted <ted10401@gmail.com>
2019-06-15 11:14:07 +0800

    Add the dynamic procedure cave.

--- Assets/04.DynamicProceduralCave/DynamicProceduralCave.cs
+++ Assets/04.DynamicProceduralCave/DynamicProceduralCave.cs
@@ -4,14 +4,23 @@ public class DynamicProceduralCave : MonoBehaviour
 {
     public Vector2Int mapSize = new Vector2Int(10, 10);
     [Range(0f, 1f)] public float cullingValue = 0f;
+    [Range(0, 10)] public int smoothInteration = 0;
+    [Range(0, 8)] public int smoothThreshold = 4;
     public float cubeSize = 1f;
 
     private float[,] m_floatMaps;
     private float[,] m_cullingMaps;
+    private int[,] m_finalMaps;
 
     private void OnValidate()
     {
         GenerateCullingMap();
+        GenerateFinalMap();
+    }
+
+    private void Awake()
+    {
+        GenerateMap();
     }
 
     private void Update()
@@ -26,6 +35,7 @@ public class DynamicProceduralCave : MonoBehaviour
     {
         GenerateFloatMap();
         GenerateCullingMap();
+        GenerateFinalMap();
     }
 
     private void GenerateFloatMap()
@@ -36,7 +46,14 @@ public class DynamicProceduralCave : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                m_floatMaps[x, y] = Random.Range(0f, 1f);
+                if (x == 0 || x == mapSize.x - 1 || y == 0 || y == mapSize.y - 1)
+                {
+                    m_floatMaps[x, y] = 1;
+                }
+                else
+                {
+                    m_floatMaps[x, y] = Random.Range(0f, 1f);
+                }
             }
         }
     }
@@ -59,6 +76,79 @@ public class DynamicProceduralCave : MonoBehaviour
         }
     }
 
+    private void GenerateFinalMap()
+    {
+        if(smoothInteration <= 0)
+        {
+            return;
+        }
+
+        m_finalMaps = new int[mapSize.x, mapSize.y];
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                m_finalMaps[x, y] = m_cullingMaps[x, y] >= cullingValue ? 1 : 0;
+            }
+        }
+
+        for (int i = 0; i < smoothInteration; i++)
+        {
+            bool[,] needUpdate = new bool[mapSize.x, mapSize.y];
+
+            for (int x = 0; x < mapSize.x; x++)
+            {
+                for (int y = 0; y < mapSize.y; y++)
+                {
+                    int wallCount = GetWallCount(m_finalMaps, x, y);
+                    if (wallCount > smoothThreshold)
+                    {
+                        needUpdate[x, y] = m_finalMaps[x, y] == 0;
+                    }
+                    else
+                    {
+                        needUpdate[x, y] = m_finalMaps[x, y] == 1;
+                    }
+                }
+            }
+
+            for (int x = 0; x < mapSize.x; x++)
+            {
+                for (int y = 0; y < mapSize.y; y++)
+                {
+                    if (needUpdate[x, y])
+                    {
+                        m_finalMaps[x, y] = m_finalMaps[x, y] == 1 ? 0 : 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private int GetWallCount(int[,] maps, int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int x = gridX - 1; x <= gridX + 1; x++)
+        {
+            for (int y = gridY - 1; y <= gridY + 1; y++)
+            {
+                if (x >= 0 && x < mapSize.x && y >= 0 && y < mapSize.y)
+                {
+                    if (x != gridX || y != gridY)
+                    {
+                        wallCount += maps[x, y];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
     private void OnDrawGizmos()
     {
         if(m_floatMaps == null)
@@ -70,11 +160,20 @@ public class DynamicProceduralCave : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Color color = Color.white * (1 - m_cullingMaps[x, y]);
-                color.a = 1;
-                Gizmos.color = color;
+                if(smoothInteration == 0)
+                {
+                    Color color = Color.white * (1 - m_cullingMaps[x, y]);
+                    color.a = 1;
+                    Gizmos.color = color;
+                }
+                else
+                {
+                    Color color = Color.white * (1 - m_finalMaps[x, y]);
+                    color.a = 1;
+                    Gizmos.color = color;
+                }
 
-                Gizmos.DrawCube(new Vector3((float)-mapSize.x / 2 + x + cubeSize / 2, (float)-mapSize.y / 2 + y + cubeSize / 2, 0), Vector3.one * cubeSize);
+                Gizmos.DrawSphere(new Vector3((float)-mapSize.x / 2 + x + cubeSize / 2, (float)-mapSize.y / 2 + y + cubeSize / 2, 0), cubeSize / 2);
             }
         }
     }
